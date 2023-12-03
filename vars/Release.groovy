7af5beefb2eef52cb61b47e7e5d239bb47b70ddb
evaldasguogis <evaldasguogis@yahoo.com>
2023-12-03 20:21:38 +0200

    grrrrrrrrrr

--- vars/Release.groovy
+++ vars/Release.groovy
@@ -45,18 +45,18 @@ def call() {
                         codeArtifact = new CodeArtifact(env.AWS_DOMAIN, env.AWS_DOMAIN_OWNER, env.AWS_DEFAULT_REGION, env.AWS_CREDENTIALS_ID)
                         env.CODEARTIFACT_AUTH_TOKEN = codeArtifact.generateToken()
 
-                        if (credentials('your-aws-credentials-id')) {
+                        if (credentials(env.AWS_CREDENTIALS_ID)) {
                             println 'Credentials found'
                         } else {
                             println 'Credentials not found'
                         }
 
-                        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: env.AWS_CREDENTIALS_ID]]) {
-                            sh 'printenv'
-                            if (codeArtifact.hasPackage('releases', pom.groupId, pom.artifactId, pom.version)) {
-                                error("Release version already exists in the repository.")
-                            }
-                        }
+//                        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: env.AWS_CREDENTIALS_ID]]) {
+//                            sh 'printenv'
+////                            if (codeArtifact.hasPackage('releases', pom.groupId, pom.artifactId, pom.version)) {
+////                                error("Release version already exists in the repository.")
+////                            }
+//                        }
                     }
                 }
 
